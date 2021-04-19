763973602261db297dfb66a8b1044578b2991844
Nishant Chapa <nishant.chapa@appdynamics.com>
2021-04-19 15:47:19 -0700

    adding property binding and event binding examples

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+
+  title = "testing new component";
+  serverNumber: number = 10;
+  serverStatus: String = "offline";
+  allowNewServer = false;
+  serverCreationStatus = "No server is available";
+
+  constructor(){
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 5000);
+  }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = "Server was created";
+  }
+
+}
